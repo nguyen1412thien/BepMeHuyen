d2ff7ebd1b97ca384b48b92992ea08ad55457369
14 Dec <nt.thien1412@icloud.com>
2026-05-19 13:00:29 +0700

    configure backend PORT=5500 and build frontend directly to server public folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../server/public'),
+    emptyOutDir: true
+  },
   server: {
-    port: 5500,
+    port: 3000,
     host: true // Cho phép truy cập mạng cục bộ
   }
 });
